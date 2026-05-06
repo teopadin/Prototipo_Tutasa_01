470da3329a820a46976aba5231b0f1ebb77d96d9
MARIA BELEN FILINICH <belenfilinich@gmail.com>
2026-05-06 16:38:24 -0300

    Hoja de ruta de distribución

---
+++ Prototipos_TUTASA/Distribución_Hdr_.cs
@@ -0,0 +1,20 @@
+﻿namespace Prototipos_TUTASA
+{
+    public partial class Distribucion_Hdr_ : Form
+    {
+        public Distribucion_Hdr_()
+        {
+            InitializeComponent();
+        }
+
+        private void lblGuiaBase_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
