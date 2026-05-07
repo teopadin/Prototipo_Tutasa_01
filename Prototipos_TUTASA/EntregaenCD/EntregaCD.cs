ee20331fb1c92cd23485527b38dd2267e7d6db4b
teopa <teopa@VMCAI>
2026-05-06 23:26:05 -0300

    Subo carpeta de Gen. HDR retiro v2

--- Prototipos_TUTASA/EntregaenCD/EntregaCD.cs
+++ Prototipos_TUTASA/EntregaenCD/EntregaCD.cs
@@ -8,21 +8,11 @@ using System.Windows.Forms;
 
 namespace Prototipos_TUTASA.EntregaenCD
 {
-    public partial class Form1 : Form
+    public partial class EntregaCD : Form
     {
-        public Form1()
+        public EntregaCD()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
---
+++ Prototipos_TUTASA/GeneracionHoja DeRutaRetiro_v2/GeneracionHojaDeRutaRetiro_v2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prototipos_TUTASA.GeneracionHoja_DeRutaRetiro_v2
+{
+    public partial class GeneracionHojaDeRutaRetiro_v2 : Form
+    {
+        public GeneracionHojaDeRutaRetiro_v2()
+        {
+            InitializeComponent();
+        }
+    }
+}
