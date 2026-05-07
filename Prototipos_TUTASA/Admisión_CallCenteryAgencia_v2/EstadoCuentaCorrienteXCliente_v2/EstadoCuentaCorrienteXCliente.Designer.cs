c9a8b28c1426f2ef4a675e44eeaba3bcc6bcc0da
BRUNO GARCÍA FRANCO <73GA45497355@campus.economicas.uba.ar>
2026-05-07 19:12:53 -0300

    estado cc cliente

---
+++ Prototipos_TUTASA/Admisión_CallCenteryAgencia_v2/EstadoCuentaCorrienteXCliente_v2/EstadoCuentaCorrienteXCliente.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Prototipos_TUTASA.Admisión_CallCenteryAgencia_v2.EstadoCuentaCorrienteXCliente_v2
+{
+    partial class EstadoCuentaCorrienteXCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTitulo = new Label();
+            SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.Font = new Font("Segoe UI", 16F);
+            lblTitulo.ForeColor = Color.FromArgb(30, 30, 30);
+            lblTitulo.Location = new Point(12, 9);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(587, 41);
+            lblTitulo.TabIndex = 40;
+            lblTitulo.Text = "Estado de Cuenta Corriente por Cliente";
+            // 
+            // EstadoCuentaCorrienteXCliente
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(974, 785);
+            Controls.Add(lblTitulo);
+            Name = "EstadoCuentaCorrienteXCliente";
+            Text = "Estado de CC por Cliente";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label lblTitulo;
+    }
+}
