dbedf7b9c6defe5d8b2c7f478b280d31bd5f087b
Cami Blanco <camiblanco140599@gmail.com>
2026-05-07 21:06:47 -0300

    Cambio

--- Prototipos_TUTASA/Generación_Resumen_HDR/Resumen_HDR_Retiro.Designer.cs
+++ Prototipos_TUTASA/Generación_Resumen_HDR/Resumen_HDR_Retiro.Designer.cs
@@ -36,13 +36,13 @@
             textBox3 = new TextBox();
             Fletero = new Label();
             listView1 = new ListView();
-            label4 = new Label();
             HojRut = new ColumnHeader();
             Remitente = new ColumnHeader();
             Calle = new ColumnHeader();
             Altura = new ColumnHeader();
             CP = new ColumnHeader();
             TipoBulto = new ColumnHeader();
+            label4 = new Label();
             textBox4 = new TextBox();
             textBox5 = new TextBox();
             label6 = new Label();
@@ -55,164 +55,179 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 23);
+            label1.Location = new Point(17, 38);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(314, 30);
+            label1.Size = new Size(481, 45);
             label1.TabIndex = 0;
             label1.Text = "Resumen Hoja de Ruta de Retiro";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(21, 78);
+            label2.Location = new Point(30, 130);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(95, 15);
+            label2.Size = new Size(144, 25);
             label2.TabIndex = 1;
             label2.Text = "CD Emisor (FIJO)";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(21, 96);
+            textBox1.Location = new Point(30, 160);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(141, 31);
             textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(165, 96);
+            textBox2.Location = new Point(236, 160);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
+            textBox2.Size = new Size(141, 31);
             textBox2.TabIndex = 4;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(165, 78);
+            label3.Location = new Point(236, 130);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(38, 15);
+            label3.Size = new Size(57, 25);
             label3.TabIndex = 3;
             label3.Text = "Fecha";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(316, 96);
+            textBox3.Location = new Point(451, 160);
+            textBox3.Margin = new Padding(4, 5, 4, 5);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
+            textBox3.Size = new Size(141, 31);
             textBox3.TabIndex = 6;
             // 
             // Fletero
             // 
             Fletero.AutoSize = true;
-            Fletero.Location = new Point(316, 78);
+            Fletero.Location = new Point(451, 130);
+            Fletero.Margin = new Padding(4, 0, 4, 0);
             Fletero.Name = "Fletero";
-            Fletero.Size = new Size(94, 15);
+            Fletero.Size = new Size(144, 25);
             Fletero.TabIndex = 5;
             Fletero.Text = "Fletero asignado";
             // 
             // listView1
             // 
             listView1.Columns.AddRange(new ColumnHeader[] { HojRut, Remitente, Calle, Altura, CP, TipoBulto });
-            listView1.Location = new Point(21, 172);
+            listView1.Location = new Point(30, 287);
+            listView1.Margin = new Padding(4, 5, 4, 5);
             listView1.Name = "listView1";
-            listView1.Size = new Size(419, 97);
+            listView1.Size = new Size(659, 159);
             listView1.TabIndex = 7;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
             // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(21, 144);
-            label4.Name = "label4";
-            label4.Size = new Size(226, 15);
-            label4.TabIndex = 8;
-            label4.Text = "Hoja de Ruta de Retiro asignada al Fletero";
-            // 
             // HojRut
             // 
             HojRut.Text = "Nro de Hoja de Retiro";
-            HojRut.Width = 130;
+            HojRut.Width = 200;
             // 
             // Remitente
             // 
             Remitente.Text = "Remitente";
-            Remitente.Width = 70;
+            Remitente.Width = 100;
             // 
             // Calle
             // 
             Calle.Text = "Calle";
-            Calle.Width = 40;
+            Calle.Width = 50;
             // 
             // Altura
             // 
             Altura.Text = "Altura";
-            Altura.Width = 50;
+            Altura.Width = 70;
             // 
             // CP
             // 
             CP.Text = "CP";
-            CP.Width = 30;
+            CP.Width = 80;
             // 
             // TipoBulto
             // 
             TipoBulto.Text = "Cant de Bultos";
-            TipoBulto.Width = 90;
+            TipoBulto.Width = 150;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(30, 240);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(344, 25);
+            label4.TabIndex = 8;
+            label4.Text = "Hoja de Ruta de Retiro asignada al Fletero";
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(21, 330);
+            textBox4.Location = new Point(30, 502);
+            textBox4.Margin = new Padding(4, 5, 4, 5);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(136, 23);
+            textBox4.Size = new Size(193, 31);
             textBox4.TabIndex = 10;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(262, 330);
+            textBox5.Location = new Point(264, 502);
+            textBox5.Margin = new Padding(4, 5, 4, 5);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(115, 23);
+            textBox5.Size = new Size(163, 31);
             textBox5.TabIndex = 12;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(262, 312);
+            label6.Location = new Point(264, 472);
+            label6.Margin = new Padding(4, 0, 4, 0);
             label6.Name = "label6";
-            label6.Size = new Size(115, 15);
+            label6.Size = new Size(172, 25);
             label6.TabIndex = 11;
             label6.Text = "Total Bultos a Retirar";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(21, 312);
+            label5.Location = new Point(30, 472);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(136, 15);
+            label5.Size = new Size(204, 25);
             label5.TabIndex = 9;
             label5.Text = "Total Domicilios a Visitar";
             // 
             // button1
             // 
-            button1.Location = new Point(84, 400);
+            button1.Location = new Point(123, 563);
+            button1.Margin = new Padding(4, 5, 4, 5);
             button1.Name = "button1";
-            button1.Size = new Size(119, 44);
+            button1.Size = new Size(170, 73);
             button1.TabIndex = 13;
             button1.Text = "Generar Resumen";
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(246, 400);
+            button2.Location = new Point(358, 563);
+            button2.Margin = new Padding(4, 5, 4, 5);
             button2.Name = "button2";
-            button2.Size = new Size(119, 44);
+            button2.Size = new Size(170, 73);
             button2.TabIndex = 14;
             button2.Text = "Cancelar";
             button2.UseVisualStyleBackColor = true;
             // 
             // Resumen_HDR_Retiro
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(604, 465);
+            ClientSize = new Size(699, 676);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(textBox5);
@@ -228,6 +243,7 @@
             Controls.Add(textBox1);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Resumen_HDR_Retiro";
             Text = "ResumenHDR";
             Load += ResumenHDR_Load;
