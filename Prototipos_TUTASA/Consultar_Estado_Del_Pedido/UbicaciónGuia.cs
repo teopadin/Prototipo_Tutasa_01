d9f079236cf6fef49127857b020dbe98bbc0baf9
Cami Blanco <camiblanco140599@gmail.com>
2026-05-30 10:08:10 -0300

    Cambios

---
+++ Prototipos_TUTASA/Consultar_Estado_Del_Pedido/UbicaciónGuia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototipos_TUTASA.Consultar_Estado_Del_Pedido
+{
+    public enum UbicaciónGuia
+    {
+        EnCDOrigen,
+        ConTransportistaLocal,
+        EnServicioMediaDistancia,
+        EnCDDestino,
+        EnAgencia
+    }
+}
