a407f49b1ddb4e3224ddb349020a21fb7e3b4e9e
MARIA BELEN FILINICH <belenfilinich@gmail.com>
2026-05-30 20:09:14 -0300

    clases de generacion de hdr distribucion

---
+++ Prototipos_TUTASA/Generación_HDR/Generación_Hoja_De_Ruta_De_Distribucion/DestinatrioEntidad.cs
@@ -0,0 +1,14 @@
+﻿namespace Prototipos_TUTASA.Generación_HDR.Generación_Hoja_De_Ruta_De_Distribucion
+{
+    internal class DestinatarioEntidad
+    {
+        public int Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Calle { get; set; }
+        public int Altura { get; set; }
+        public string Piso { get; set; }
+        public string CodigoPostal { get; set; }
+        public string Ciudad { get; set; }
+    }
+}
---
+++ Prototipos_TUTASA/Generación_HDR/Generación_Hoja_De_Ruta_De_Distribucion/EstadoGuia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototipos_TUTASA.Generación_HDR.Generación_Hoja_De_Ruta_De_Distribucion
+{
+    internal enum EstadoGuia
+    {
+        EnCDDestino,
+        EnDistribucion
+    }
+}
