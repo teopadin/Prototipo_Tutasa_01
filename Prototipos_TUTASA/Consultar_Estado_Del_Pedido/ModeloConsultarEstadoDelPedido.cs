6f9fe197ec39d88b7cc837c7c998fe4723afa537
Cami Blanco <camiblanco140599@gmail.com>
2026-05-30 12:01:22 -0300

    Cambios

--- Prototipos_TUTASA/Consultar_Estado_Del_Pedido/ModeloConsultarEstadoDelPedido.cs
+++ Prototipos_TUTASA/Consultar_Estado_Del_Pedido/ModeloConsultarEstadoDelPedido.cs
@@ -15,16 +15,16 @@ namespace Prototipos_TUTASA.Consultar_Estado_Del_Pedido
                 Estado = EstadoGuia.Entregada,
                 Historial = new List<HistorialEstadoGuiaEntidad>
                 {
-                    new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 10), Estado = EstadoGuia.Impuesta, Donde = UbicacionGuia.EnCDOrigen },
-                    new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 11), Estado = EstadoGuia.PendienteDeRetiroPorTransportista, Donde = UbicacionGuia.EnCDOrigen },
-                    new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 12), Estado = EstadoGuia.Retirada, Donde = UbicacionGuia.ConTransportistaLocal },
-                    new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 13), Estado = EstadoGuia.Admitida, Donde = UbicacionGuia.EnCDOrigen },
-                    new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 14), Estado = EstadoGuia.PendienteDeDespacho, Donde = UbicacionGuia.EnCDOrigen },
-                    new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 15), Estado = EstadoGuia.EnTransito, Donde = UbicacionGuia.EnServicioMediaDistancia },
-                    new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 17), Estado = EstadoGuia.EnCDDestino, Donde = UbicacionGuia.EnCDDestino },
-                    new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 18), Estado = EstadoGuia.EnDistribucion, Donde = UbicacionGuia.ConTransportistaLocal },
-                    new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 19), Estado = EstadoGuia.PendienteDeRetiroEnAgencia, Donde = UbicacionGuia.EnAgencia },
-                    new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 21), Estado = EstadoGuia.Entregada, Donde = UbicacionGuia.EnAgencia }
+                        new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 10), Estado = EstadoGuia.Impuesta, Donde = UbicacionGuia.EnAgencia },
+                        new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 11), Estado = EstadoGuia.PendienteDeRetiroPorTransportista, Donde = UbicacionGuia.EnAgencia },
+                        new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 12), Estado = EstadoGuia.Retirada, Donde = UbicacionGuia.ConTransportistaLocal },
+                        new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 13), Estado = EstadoGuia.Admitida, Donde = UbicacionGuia.EnCDOrigen },
+                        new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 14), Estado = EstadoGuia.PendienteDeDespacho, Donde = UbicacionGuia.EnCDOrigen },
+                        new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 15), Estado = EstadoGuia.EnTransito, Donde = UbicacionGuia.EnServicioMediaDistancia },
+                        new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 17), Estado = EstadoGuia.EnCDDestino, Donde = UbicacionGuia.EnCDDestino },
+                        new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 18), Estado = EstadoGuia.EnDistribucion, Donde = UbicacionGuia.ConTransportistaLocal },
+                        new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 19), Estado = EstadoGuia.PendienteDeRetiroEnAgencia, Donde = UbicacionGuia.EnAgencia },
+                        new HistorialEstadoGuiaEntidad { FechaCambio = new DateTime(2026, 5, 21), Estado = EstadoGuia.Entregada, Donde = UbicacionGuia.EnAgencia }
                 }
             },
             // Guía CD02-0002 - Retiro en CD destino
