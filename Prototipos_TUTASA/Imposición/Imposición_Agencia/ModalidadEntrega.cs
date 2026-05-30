10d3b7c92cbbdae632b199b821dd8df0e057d97a
teopa <teopa@VMCAI>
2026-05-30 20:31:40 -0300

    tuftff

---
+++ Prototipos_TUTASA/Imposición/Imposición_Agencia/ModalidadEntrega.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototipos_TUTASA.Imposición.Imposición_Agencia
+{
+    internal enum ModalidadEntrega
+    {
+        PuertaPuerta,
+        RetiroAgencia,
+        RetiroCD
+    }
+}
