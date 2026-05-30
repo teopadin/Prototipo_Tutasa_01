24e4e665fbc50920c339e2709fba13f3836807f9
Cami Blanco <camiblanco140599@gmail.com>
2026-05-30 09:35:55 -0300

    Cambios ConsultaEstado

---
+++ Prototipos_TUTASA/Consultar_Estado_Del_Pedido/GuiaEntidad.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototipos_TUTASA.Consultar_Estado_Del_Pedido
+{
+    internal class GuiaEntidad
+    {
+    }
+}
---
+++ Prototipos_TUTASA/Consultar_Estado_Del_Pedido/HistorialEstadoGuiaEntidad.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototipos_TUTASA.Consultar_Estado_Del_Pedido
+{
+    public class HistorialEstadoGuiaEntidad
+    {
+        public DateTime FechaCambio { get; set; }
+        public EstadoGuia Estado { get; set; }
+    }
+}
