cbd683291ab6d44cc5e9e16dcde0c3fed72afae7
BRUNO GARCÍA FRANCO <73GA45497355@campus.economicas.uba.ar>
2026-06-01 00:12:58 -0300

    prueba program

--- Prototipos_TUTASA/Program.cs
+++ Prototipos_TUTASA/Program.cs
@@ -38,7 +38,7 @@ namespace Prototipos_TUTASA
             //Application.Run(new ConsultarEstadoDelPedido_v2.ConsultarEstadoDelPedido());
             //Application.Run(new resultadocostosvsventas());
             //Application.Run(new Prototipos_TUTASA.Admisión_CallCenteryAgencia_v2.EstadoCuentaCorrienteXCliente_v2.EstadoCuentaCorrienteXCliente());
-            Application.Run(new EntregaAgencia());
+            //Application.Run(new EntregaAgencia());
         }
     }
 }
