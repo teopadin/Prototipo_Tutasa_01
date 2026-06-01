edb25c6938bedd858e4b414b37eea9f8812a29b9
BRUNO GARCÍA FRANCO <73GA45497355@campus.economicas.uba.ar>
2026-06-01 20:17:50 -0300

    Cambio recepcion hdr transporte

--- Prototipos_TUTASA/Program.cs
+++ Prototipos_TUTASA/Program.cs
@@ -27,7 +27,7 @@ namespace Prototipos_TUTASA
             //Application.Run(new Prototipos_TUTASA.RecepcionHojaDeRutaDeRetiroEnAgencia.Recepcion_HDR_Retiro_Agencia());
             //Application.Run(new Rendir_HDR_De_Retiro());
             //Application.Run(new Admisión.Admisión());
-            //Application.Run(new GenerarHojaDeRutaDeTransporte());
+            Application.Run(new GenerarHojaDeRutaDeTransporte());
             //Application.Run(new Despacho_Servicios_Media_Distancia.Despacho_Servicios_Media_Distancia());
             //Application.Run(new Prototipos_TUTASA.RecepcionMediaDistancia.RecepcionMediaDistancia());
             //Application.Run(new HojaDeRutaDeDistribucionDestinatario());
