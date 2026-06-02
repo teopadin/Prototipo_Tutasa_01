822d2361429e8a1296e0571fc27966bd55bcda85
BRUNO GARCÍA FRANCO <73GA45497355@campus.economicas.uba.ar>
2026-06-02 00:02:51 -0300

    clases RHDRMD

---
+++ Prototipos_TUTASA/RecepcionMediaDistancia/HojaDeRutaTransporte.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototipos_TUTASA.RecepcionMediaDistancia
+{
+    internal class HojaDeRutaTransporte
+    {
+        public int NroHDR { get; set; }
+        public EstadoHojaDeRuta Estado { get; set; }
+        public List<Guia> Guias { get; set; }
+    }
+}
