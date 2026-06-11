5ee82d0a61d3b3c50e4756cc0c6c2092e1e1a2d5
MARIA BELEN FILINICH <belenfilinich@gmail.com>
2026-06-11 00:31:31 -0300

    cambios en admnistracion

---
+++ Prototipos_TUTASA/Administración/Resultado_Costo_VS_Ventas/TarifaCalculadaGuia.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototipos_TUTASA.ResultadoCostoVSVentas_v2
+{
+    internal class TarifaCalculadaGuia
+    {
+        public decimal TarifaTotal { get; set; }
+    }
+}
